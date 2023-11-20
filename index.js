574cf4e0e465115a17c44460c06071103d895349
Stanislav Anuprienko <anuper@yandex.ru>
2023-11-20 15:56:01 +0300

    js

--- index.js
+++ index.js
@@ -20,3 +20,44 @@ new Swiper('.swiper', {
     loop: true,
     centereSlide: true,
 });
+
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
+
+if (ScrollTrigger.isTouch !== 1) {
+
+    ScrollSmoother.create ({
+        wrapper: 'wrapper',
+        content: 'content',
+        smooth: 3,
+        effects: true,
+    })
+
+    gsap.fromTo('.advantages__block-left', { x: -100, opacity: 0 }, {
+        opacity: 1, x: 0,
+        scrollTrigger: {
+            trigger: '.advantages__icon',
+            start: '-850',
+		
+    
+        }
+    })
+
+    gsap.fromTo('.advantages__block-right', { x: +100, opacity: 0 }, {
+        opacity: 1, x: 0,
+        scrollTrigger: {
+            trigger: '.advantages__icon',
+            start: '-850',
+
+        }
+    })
+
+    gsap.fromTo('.footer', { y: -100, opacity: 0 }, {
+        opacity: 1, y: 0,
+        scrollTrigger: {
+            trigger: '.footer',
+            start: '-850',
+    
+        }
+    })
+}
+
